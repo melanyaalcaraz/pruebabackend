24120908f834b6bfad0c4adc3d8618521837a623
Melany Alcaraz <103854523+melanyaalcaraz@users.noreply.github.com>
2025-04-21 18:29:03 -0300

    prueba

---
+++ api/ranking.js
@@ -0,0 +1,19 @@
+let ranking = [];
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const nuevaPartida = req.body;
+
+    ranking.push(nuevaPartida);
+    ranking.sort((a, b) => b.puntos - a.puntos);
+    ranking = ranking.slice(0, 20);
+
+    return res.status(201).json({ mensaje: 'Partida guardada' });
+  }
+
+  if (req.method === 'GET') {
+    return res.status(200).json(ranking);
+  }
+
+  return res.status(405).json({ error: 'Método no permitido' });
+}
